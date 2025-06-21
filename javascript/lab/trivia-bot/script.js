78843b7e66571412947184172a7baf5e10c5188c
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-06-21 19:54:23 -0300

    Add trivia-bot

---
+++ javascript/lab/trivia-bot/script.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Paul";
+let botLocation = "England";
+let favoriteLanguage = "Java";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "I like coding in " + favoriteLanguage + "!";
+console.log(codingFact);
+
+codingFact = "I know how to do a lot of cool things with " + favoriteLanguage;
+console.log(codingFact);
+
+codingFact = "For me " + favoriteLanguage + " is the best language for the job market.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
