8bf8d57547f2088128b51b0bf9e51392e400624b
Luiz Henrique Bueno <luizbueno.dev@gmail.com>
2025-06-22 11:38:46 -0300

    Add sentence-maker

---
+++ javascript/lab/sentence-maker/script.js
@@ -0,0 +1,20 @@
+let adjective = "resilient";
+let noun = "researcher";
+let verb = "exposed to chemicals";
+let place = "remote laboratory";
+let adjective2 = "sensitive";
+let noun2 = "granola";
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+adjective = "dedicated";
+noun = "engineer";
+verb = "stressed";
+place = "small apartment";
+adjective2 = "sensitive";
+noun2 = "almonds";
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+console.log("Second story: " + secondStory);
